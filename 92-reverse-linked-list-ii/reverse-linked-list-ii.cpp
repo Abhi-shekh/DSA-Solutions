4d6c74134e62991bc09cdc6856ccc1f55e3787c2
Abhishek Raj <96643102+Abhi-shekh@users.noreply.github.com>
2026-02-17 11:50:59 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (71.19%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* revLL(ListNode* &left, ListNode* &right){
+        ListNode* curr=left;
+        ListNode* prev=NULL;
+        ListNode* stop=right->next;
+
+        while(curr!=stop){
+            ListNode* frr=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=frr;
+        }
+        return prev;
+    }
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode* dummy= new ListNode(0);
+        dummy->next=head;
+        ListNode* prev=dummy;
+
+        while(left>1){
+            prev=prev->next;
+            left--;
+            right--;
+        }
+        ListNode* leftNode=prev->next;
+        ListNode* rightNode=prev;
+
+        while(right>0){
+            rightNode=rightNode->next;
+            right--;
+        }
+        ListNode* frr=rightNode->next;
+
+        ListNode* rev=revLL(leftNode, rightNode);
+        prev->next=rev;
+        leftNode->next=frr;
+
+        return dummy->next;
+
+        
+    }
+};
